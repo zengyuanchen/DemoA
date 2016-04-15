0e1c1a6d0788dad72168ff8b761323b0ca5b81bc
zengyuanchen <zengyuanchen@21cn.com>
2016-04-15 14:12:18 +0800

    Initial Commit

---
+++ DemoA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoA
+//
+//  Created by 曾源琛 on 16/4/15.
+//  Copyright © 2016年 zengyuanchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoA
+//
+//  Created by 曾源琛 on 16/4/15.
+//  Copyright © 2016年 zengyuanchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
